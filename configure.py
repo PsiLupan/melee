6515f31985f94481ce1540dccc4fbcaa76ce7859
Will C <wcarter312@gmail.com>
2024-09-16 23:41:48 -0400

    Move pending code out to a directory to avoid it conflicting

--- configure.py
+++ configure.py
@@ -1263,6 +1263,7 @@ config.libs = [
         [
             Object(NonMatching, "dolphin/thp/THPDec.c"),
         ],
+        extern=True,
     ),
     DolphinLib(
         "base",
@@ -1297,6 +1298,7 @@ config.libs = [
             Object(NonMatching, "dolphin/dvd/dvderror.c"),
             Object(Matching, "dolphin/dvd/fstload.c"),
         ],
+        extern=True,
     ),
     DolphinLib(
         "gx",
@@ -1317,6 +1319,7 @@ config.libs = [
             Object(NonMatching, "dolphin/gx/GXTransform.c"),
             Object(NonMatching, "dolphin/gx/GXPerf.c"),
         ],
+        extern=True,
     ),
     DolphinLib(
         "mtx",
